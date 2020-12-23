b0a023ed3d3a7cf1875f66bbd4974f3db2cd6c36
Joshua Haddock <joshhaddock88@gmail.com>
2020-12-23 06:13:24 -0800

    Stop game at end of fifth round and declare winner. Add reset button w/no functionality

--- script.js
+++ script.js
@@ -2,11 +2,40 @@
       /**So, what if I assigned rock paper scissors number values, and depend **/
 
 
-
+//Universal Variables
+//Keeps track of score
+let round = 0;
+let win = 0;
+let lose = 0;
+let tie = 0;
 const rock = document.getElementById('rock').value;
 const paper = document.getElementById('paper').value;
 const scissors = document.getElementById('scissors').value;
 const buttons = document.querySelectorAll('button');
+//A loop to tall rounds.
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        singleGame(button.value);
+        playSound(button.value);
+    })
+})
+
+function playSound(id) {
+    const rockAudio = document.querySelector('#rockSound');
+    const paperAudio = document.querySelector('#paperSound');
+    const scissorsAudio = document.querySelector('#scissorsSound');
+    if(round === 5) {
+        return(false);
+    } else {
+    if(id === rock) {
+        rockAudio.play();
+    } else if(id === paper) {
+        paperAudio.play();
+    } else if(id === scissors) {
+        scissorsAudio.play();
+    }
+}
+}
 
 function computerPlay(randomChoice) {
     let choices = [rock, paper, scissors];
@@ -24,72 +53,69 @@ function singleGame(userSelection) {
     const computer = document.getElementById('computer');
     computer.innerText = `${computerSelection}`;
     const outcome = document.getElementById('outcome');
-    const winMessage = `You win!`;
-    const tieMessage = `It's a tie!`
+    const roundNumber = document.getElementById('round');
+    const winNumber = document.getElementById('win');
+    const lossNumber = document.getElementById('loss');
+    const tieNumber = document.getElementById('tie');
+    
 
-    if (userSelection === rock) {
-        if (computerSelection === paper) {
-            outcome.innerText = 'You lose!'
-            return('lose')
-        } else if (computerSelection === scissors) {
-            outcome.innerText = 'You win!'
-            return('win');
-        } else if (computerSelection === rock) {
-            outcome.textContent = `It's a tie!`;
-            return('tie');
+    if(round === 5) {
+        intro.innerText = 'Game Over';
+        if(win > lose) {
+            computer.innerText = 'You have won!';
+            outcome.innerText = 'Congratulations!';
+        } else if (lose > win) {
+            computer.innerText = 'Oh, you lost...';
+            outcome.innerText = 'I am so, so sorry...';
+        } else if (tie > win && tie > lose) {
+            computer.innerText = 'My, my, my,'
+            outcome.innerText = 'Somehow you tied...'
         }
-    } else if (userSelection === paper) {
-        if (computerSelection === scissors) {
-            outcome.innerText = 'You lose!'
-            return('lose')
-        } else if (computerSelection === rock) {
-            outcome.innerText = 'You win!';
-            return('win');
-        } else if (computerSelection === paper) {
-            outcome.innerText = `It's a tie!`;
-            return('tie');
-        }
-    } else if (userSelection === scissors) {
-        if (computerSelection === rock) {
-            outcome.innerText = 'You lose!'
-            return('lose')
-        } else if (computerSelection === paper) {
-            outcome.innerText = 'You win!';
-            return('win');
-        } else if (computerSelection === scissors) {
-            outcome.innerText = `It's a tie!`;
-            return('tie');
-        }
-    }
-}
-
-//Create a function that takes singleGame and iterates through it for 5 consecutive rounds
-//At the beginning of each round it will output the round number and then run through the entire singGame function
-//Alert user of winner
-function fiveRoundGame(oneGame) {
-    let win = 0;
-    let lose = 0;
-    //This tie option is kinda superflous. I just left it here in case I want to keep track of ties later down the road. But currenlty it's not being used.
-    let tie = 0;
-    for (round = 1; round <= 5; round++) {
-        alert(`Round ${round}!`);
-        let outCome = singleGame(userPlay(), computerPlay());
-        if (outCome === 'win') {
-            win += 1;
-        } else if (outCome === 'lose') {
-            lose += 1;
-        } else if (outCome === 'tie') {
-            tie += 1;
-        }
-        //endOfRound function has to go at end of loop, otherwise the score in the alert will always be one round behind
-        let endOfRound = `End of Round ${round}! The score is Computer: ${lose} vs User: ${win}`
-        alert(endOfRound);
-    }
-    if (win > lose) {
-        alert("You win!");
-    } else if (win === lose) {
-        alert("It's a tie!");
     } else {
-        alert("You lose!");
+        if (userSelection === rock) {
+            if (computerSelection === paper) {
+                outcome.innerText = 'You lose!'
+                lose += 1;
+                lossNumber.innerText = `Lose: ${lose}`;
+            } else if (computerSelection === scissors) {
+                outcome.innerText = 'You win!'
+                win += 1;
+                winNumber.innerText = `Win: ${win}`;
+            } else if (computerSelection === rock) {
+                outcome.innerText = `It's a tie!`;
+                tie += 1;
+                tieNumber.innerText = `Tie: ${tie}`;
+            }
+        } else if (userSelection === paper) {
+            if (computerSelection === scissors) {
+                outcome.innerText = 'You lose!'
+                lose += 1;
+                lossNumber.innerText = `Lose: ${lose}`;
+            } else if (computerSelection === rock) {
+                outcome.innerText = 'You win!';
+                win += 1;
+                winNumber.innerText = `Win: ${win}`;
+            } else if (computerSelection === paper) {
+                outcome.innerText = `It's a tie!`;
+                tie += 1;
+                tieNumber.innerText = `Tie: ${tie}`;
+            }
+        } else if (userSelection === scissors) {
+            if (computerSelection === rock) {
+                outcome.innerText = 'You lose!'
+                lose += 1;
+                lossNumber.innerText = `Lose: ${lose}`;
+            } else if (computerSelection === paper) {
+                outcome.innerText = 'You win!';
+                win += 1;
+                winNumber.innerText = `Win: ${win}`;
+            } else if (computerSelection === scissors) {
+                outcome.innerText = `It's a tie!`;
+                tie += 1;
+                tieNumber.innerText = `Tie: ${tie}`;
+            }
+        }
+        round += 1;
+        roundNumber.innerText = `Round: ${round}`
     }
 }
